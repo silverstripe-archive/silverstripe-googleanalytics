d7aac1fa3daed8d24f43dcb76e63ce0d9a6d8e82
Ingo Schommer <ingo@silverstripe.com>
2009-01-12 05:02:34 +0000

    MINOR Updated language master tables

---
+++ lang/en_US.php
@@ -0,0 +1,21 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['AnalyticsReport']['LOADINGFORM'] = 'Loading Form...';
+$lang['en_US']['AnalyticsReport']['SAVED'] = 'Saved!';
+$lang['en_US']['AnalyticsReport']['SAVING'] = 'Saving...';
+$lang['en_US']['AnalyticsReport']['SETUPANALYTICS'] = 'Setup Analytics';
+$lang['en_US']['AnalyticsReport']['VIEWANALYTICS'] = 'View Google Analytics';
+$lang['en_US']['CrawlerStats']['PLURALNAME'] = array(
+	'Crawler Statss',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['CrawlerStats']['SINGULARNAME'] = array(
+	'Crawler Stats',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+
+?>
