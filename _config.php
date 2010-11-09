5a604484f4a30797967d81a968dfcf5c355b9f30
Andreas Piening <andreas@silverstripe.com>
2010-11-09 23:19:18 +0000

    MINOR: removed debugging

--- _config.php
+++ _config.php
@@ -1,6 +1,1 @@
 <?php
-
-GoogleLogger::activate('UA-84547-4');
-GoogleAnalyzer::activate('13358486', 'andreas@silverstripe.com', 's=7bNT10');
-
-// SS_Report::register("ReportAdmin", "AnalyticsReport");
